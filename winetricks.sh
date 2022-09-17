7049f4c15d267632a483818f793453f5aa58dade
iamangus <turnbull.angus@gmail.com>
2022-09-17 16:59:08 -0500

    test

--- winetricks.sh
+++ winetricks.sh
@@ -4,5 +4,5 @@ Xvfb :5 -screen 0 1024x768x16 &
 env WINEARCH=win64 WINEDEBUG=-all WINEPREFIX=/home/steam/wineserver WINEDLLOVERRIDES="mscoree=d" wineboot --init /nogui 
 env WINEARCH=win64 WINEDEBUG=-all WINEPREFIX=/home/steam/wineserver winetricks corefonts 
 env WINEARCH=win64 WINEDEBUG=-all WINEPREFIX=/home/steam/wineserver winetricks sound=disabled 
-env WINEARCH=win64 WINEDEBUG=-all WINEPREFIX=/home/steam/wineserver DISPLAY=:5.0 winetricks -q vcrun2019 
+env WINEARCH=win64 WINEDEBUG=-all WINEPREFIX=/home/steam/wineserver DISPLAY=:5.0 winetricks -q vcrun2017 
 env WINEARCH=win64 WINEDEBUG=-all WINEPREFIX=/home/steam/wineserver DISPLAY=:5.0 winetricks -q --force dotnet48
